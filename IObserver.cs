2135fde11e27d6cacbe1fffdc34960f2ac97d5ca
DANIEL MONTEIRO RIBEIRO <dmribeiro@unifesp.br>
2026-04-20 12:03:15 -0300

    Remove duplicated UNIFESPObserver class
    Removed UNIFESPObserver class implementation from IObserver.cs.

--- IObserver.cs
+++ IObserver.cs
@@ -2,23 +2,3 @@ interface IObserver
 {
     void Update(Subject subject);
 }
-
-class UNIFESPObserver : IObserver
-{
-
-    public void Update(Subject subject) 
-    {
-        switch(subject) 
-        {
-            case Temperature temp:
-                Console.WriteLine($"UNIFESP recebeu a atualização da temperatura: {temp.GetTemperature()}");
-                break;
-            case PH ph:
-                Console.WriteLine($"UNIFESP recebeu a atualização do pH: {ph.GetPH()}");
-                break;
-            case Humidity hum:
-                Console.WriteLine($"UNIFESP recebeu a atualização da umidade: {hum.GetHumidity()}");
-                break;
-        }
-    }
-}
